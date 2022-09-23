a5d80c47e3a035f328a6505426c51b0a89fb2d2a
Success <ikhinobelesuccess@gmail.com>
2022-09-23 23:04:34 +0100

    worked on the intro page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-red-200 w-full h-screen font-maven font-bold flex flex-col items-center justify-center">
+      <h1 className="text-xl md:text-3xl font-extrabold mb-8">
+        Welcome to Onose's love calculator game
+      </h1>
+
+      <Link to="/calculator">
+        <button className="w-32 h-9 border border-slate-50 rounded-md bg-transparent hover:bg-white hover:text-black">
+          Play now
+        </button>
+      </Link>
     </div>
   );
 }
