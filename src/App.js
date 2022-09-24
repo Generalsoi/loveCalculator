ed5a0b76d5e4d942110125eac00302d8d4764950
Success <ikhinobelesuccess@gmail.com>
2022-09-24 14:09:07 +0100

    added a preloader

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import { Link } from "react-router-dom";
+import { Lines } from "react-preloaders";
+import { useState, useEffect } from "react";
+import Background from "./images/love.webp";
 
 function App() {
-  return (
-    <div className="bg-red-200 w-full h-screen font-maven font-bold flex flex-col items-center justify-center">
-      <h1 className="text-xl md:text-3xl font-extrabold mb-8">
-        Welcome to Onose's love calculator game
-      </h1>
+  const [loading, setLoading] = useState(true);
 
-      <Link to="/calculator">
-        <button className="w-32 h-9 border border-slate-50 rounded-md bg-transparent hover:bg-white hover:text-black">
-          Play now
-        </button>
-      </Link>
-    </div>
+  return (
+    <>
+      {loading ? (
+        <div
+          className="w-full h-screen flex items-center justify-center"
+          style={{
+            backgroundImage: `url(${Background})`,
+            backgroundSize: "contain",
+            overflow: "hidden",
+          }}
+        >
+          {setTimeout(() => {
+            <Lines background="black" />;
+            setLoading(false);
+          }, 3000)}
+        </div>
+      ) : (
+        <div className="bg-red-200 w-full h-screen font-maven font-bold flex flex-col items-center justify-center">
+          <h1 className="text-xl md:text-3xl font-extrabold mb-8">
+            Welcome to Onose's love calculator game
+          </h1>
+
+          <Link to="/calculator">
+            <button className="w-32 h-9 border border-slate-50 rounded-md bg-transparent hover:bg-white hover:text-black">
+              Play now
+            </button>
+          </Link>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/routes/calculator.jsx
+++ src/routes/calculator.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export const Calculator = () => {
-  return <div>Calculator</div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-red-500">
+      <div className="flex flex-col md:flex-row items-center text-white">
+        <div className="flex flex-col gap-2">
+          <label htmlFor="yourName">Your name</label>
+          <input type="text" placeholder="Enter your name" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="soulMateName">Other name</label>
+          <input type="text" placeholder="Enter the other name" />
+        </div>
+      </div>
+    </div>
+  );
 };
